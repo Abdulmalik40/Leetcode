f972b2ec0c6a2988b89cd728e3bb59956274b032
Abdulmalik <abodkm05@gmail.com>
2025-09-22 23:08:02 +0300

    1978 added

---
+++ 1978.sql
@@ -0,0 +1,7 @@
+SELECT employee_id
+FROM Employees e1 
+WHERE salary < 30000 AND manager_id IS NOT NULL AND NOT EXISTS
+ (SELECT * FROM Employees e2 WHERE
+ e2.employee_id = e1.manager_id
+)
+ORDER BY employee_id
